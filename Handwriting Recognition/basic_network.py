531c359d64b952e153a105cdf941d6b3304d5679
Precious Ehihamen <34788683+Preshehi@users.noreply.github.com>
2022-08-31 16:46:02 +0100

    Update basic_network.py

--- Handwriting Recognition/basic_network.py
+++ Handwriting Recognition/basic_network.py
@@ -1,13 +1,17 @@
+#import useful libraries 
 import random
 import numpy as np
 
+#Create the network
 class Network(object):
+	#initialize the network with the specifications collected from the user
 	def __init__(self,sizes):
 		self.num_layers=len(sizes)
 		self.sizes=sizes
 		self.biases=[np.random.randn(y,1) for y in sizes[1:]]
 		self.weights=[np.random.randn(y,x) for x,y in zip(sizes[:-1], sizes[1:])]
 
+	#Propagate the network forward
 	def feedforward(self,a):
 		for b,w in zip(self.biases, self.weights):
 			a=sigmoid(np.dot(w,a)+b)
